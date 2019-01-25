35f0ae9503a1b3b0a5d4afc92da33de4e25dd469
Sarah Maples <sarah.e.maples@gmail.com>
2019-01-24 21:21:06 -0600

    for cole

--- assets/javascript/articlesearch.js
+++ assets/javascript/articlesearch.js
@@ -1,18 +1,24 @@
-$(".Search").click(function(){
-// take in all info from form and save it in variables
-    console.log("clicked");
-    var keyWords = $("input[name=searchterms]");
-    var queryURL =  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+keyWords+"&api-key=ef5156c9-019d-4e7f-9dc4-65a17d39f9af";
+$(document).ready(function(){
+    $("#search").on("click", function(e){
+        e.preventDefault();
+    // take in all info from form and save it in variables
+        console.log("clicked");
+        var keyWords = $("input[name=searchterms]");
+        console.log(keyWords);
+        var queryURL =  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=ef5156c9-019d-4e7f-9dc4-65a17d39f9af";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function(response) {
-            console.log(response);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
         })
-});
+            .then(function(response) {
+                console.log(response);
+            })
+    });
 
-$(".Clear").on("click", function(){
-    console.log("fuck you cole");
+    // $(".Clear").on("click", function(e){
+    //     e.preventDefault();
+    //     console.log("fuck you cole");
+    // });     
+
 });
