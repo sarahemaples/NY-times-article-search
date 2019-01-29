69a2ac3cf0ee4461db9ebad3b0bb77a8b60d1fee
Sarah Maples <sarah.e.maples@gmail.com>
2019-01-29 11:44:54 -0600

    headline displays in list

--- assets/javascript/articlesearch.js
+++ assets/javascript/articlesearch.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     // take in all info from form and save it in variables
         console.log("clicked");
         var keyWords = $("input[name=searchterms]").val();
-        console.log(keyWords);
+        // console.log(keyWords);
         var queryURL =  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+keyWords+"&api-key=ZV6Pg8ej8AANKrKAXnwu4ciANpJdCPky";
 
         $.ajax({
@@ -12,13 +12,48 @@ $(document).ready(function(){
             method: "GET"
         })
             .then(function(response) {
+                console.log(response.response.docs[0]);
                 console.log(response.response.docs);
+    // ----------------write a function below------------------ //
+                displayArticles(response.response.docs, 1);
             })
     });
 
     // $(".Clear").on("click", function(e){
     //     e.preventDefault();
     //     console.log("fuck you cole");
-    // });     
+    // });  
+    
+//---------------------------------------------------//
+// FUNCTION THAT DISPLAYS ARTICLES ON SCREEN
+//---------------------------------------------------//
+// this function takes in two parameters (arr, n)
+// arr is the array of articles returned
+// n is the number of articles user wishes to display. default is 10
+    function displayArticles(arr, n){
+        // clear any previously displayed articles
 
+        // create new list and append it to our 'results' div (span?)
+        var articleList = $("<ul>");
+        $("#results").append(articleList);
+
+        // for loop through the response.data array
+        for (var a = 0; a < n; a++){
+            // create new list item
+            var newArt = $("<li>");
+
+            // add headline
+            var headline = "<h2>" + arr[a].headline.main + "</h2><br>";
+            newArt.append(headline);
+
+            // add author
+
+            // add date/time
+
+            // add link to article
+
+            // add list item to list
+            articleList.append(newArt);
+        }
+    }
 });
